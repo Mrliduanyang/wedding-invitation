5193a10296d3f066d3b778a0ea7efe24217ac745
李端阳 <duanyangchn@gmail.com>
2026-04-15 09:25:04 +0000

    Update

--- src/main.js
+++ src/main.js
@@ -2662,8 +2662,8 @@ window.addEventListener("load", () => {
 
 // 婚礼现场坐标（GCJ02，腾讯/高德通用）
 const WEDDING_LOCATION = {
-  latitude: 32.990194,
-  longitude: 112.533032,
+  latitude: 32.990191,
+  longitude: 112.533027,
   name: "南阳市梅溪国际酒店",
   address: "河南省南阳市",
   scale: 16,
